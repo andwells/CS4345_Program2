334ff7193c19c2818aa97e7e4b8be2c6a4af5ee9
andwells <andwells@valdosta.edu>
2016-03-12 14:52:58 -0500

    Initial Interfaces

---
+++ src/Observer.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Andrew J. Wells 
+ * File Name: Observer.java
+ * Date: 03/12/2016
+ *
+ * Description: The Observer in the Observer pattern
+ */
+
+
+public interface Observer
+{
+	void update(Object o);	
+}
